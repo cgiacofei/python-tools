cf0e74410ddd447519e8a098b40b25e8da29c396
chris <c.giacofei@gmail.com>
2017-02-21 08:40:26 -0500

    Better package documentation.

--- setup.py
+++ setup.py
@@ -3,14 +3,28 @@
 from setuptools import setup
 
 
+def readme():
+    """Make description from repo README.md."""
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='mytools',
     version='2017.2.21a',
     description='Collection of commonly re-used python functons and classes.',
+    long_description=readme(),
+    classifiers=[
+        'License :: Public Domain',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
+        'Topic :: Utilities',
+    ],
     url='http://github.com/cgiacofei/python-tools',
     author='Chris Giacofei',
     author_email='',
-    license='unlicense',
+    license='http://unlicense.org/UNLICENSE',
     packages=['nested_dict'],
     zip_safe=False
 )
