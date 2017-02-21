b4d00b308ebc9f780d0cb4fd09b5cfa62e8394e1
chris <c.giacofei@gmail.com>
2017-02-21 07:56:08 -0500

    Initial commit.

---
+++ nested_dict/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""`nested_dict` provides dictionaries with multiple levels of nested-ness."""
+__version__ = '1.61'
+from .nested_dict import nested_dict
+
+__all__ = ('nested_dict', )
