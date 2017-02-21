f74e3f961fc19000ef3375e1543b09edacf7d787
chris <c.giacofei@gmail.com>
2017-02-21 08:09:17 -0500

    Add initial setup script.

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Setup script for mytool."""
+
+from setuptools import setup
+
+
+setup(
+    name='mytools',
+    version='2017.2.21a',
+    description='Collection of commonly re-used python functons and classes.',
+    url='http://github.com/cgiacofei/python-tools',
+    author='Chris Giacofei',
+    author_email='',
+    license='unlicense',
+    packages=['nested_dict'],
+    zip_safe=False
+)
